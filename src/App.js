4088ab27854fad1bc774b9bf28f7b2a1ff41f649
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-03-19 22:43:30 -0700

    working logic for score-keeping and game reset

--- src/App.js
+++ src/App.js
@@ -6,21 +6,67 @@ import cards from "./cards.json";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
+let score = 0;
+let highScore = 0;
+let message = "Click an image to begin!";
 
 class App extends Component {
   state = {
-    cards
+    cards,
+    score,
+    highScore,
+    message
   };
 
+  resetGame = () => (
+    cards.forEach(function(el) {
+      el.clicked = false;
+    })
+  )
+
   checkCard = (id) => {
-    this.setState({cards});
+    let cards = this.state.cards;
+    let clickedCard = cards.filter(card => card.id === id);
+    console.log(clickedCard);
+
+    if (clickedCard[0].clicked === false) {
+      // console.log("in here");
+      clickedCard[0].clicked = true;
+      score++;
+
+      if (score > highScore){
+        highScore = score;
+      }
+
+      if (score >= 12) {
+        message = "You won! Do it again."
+        score = 0;
+        this.resetGame();
+      } else {
+        message = "You guessed correctly!";
+      }
+    } else {
+      message = "You guessed incorrectly!";
+      score = 0;
+      this.resetGame();
+    }
+
+    console.log(cards);
+    this.setState({ cards });
+    this.setState({ score });
+    this.setState({ message });
+    this.setState({ highScore });
   }
 
   render() {
     return (
       <Router>
         <div>
-          <Navbar />
+          <Navbar 
+            score={score}
+            highScore={highScore}
+            message={message}
+          />
           <div className="header">
             <h1>Clicky Game!</h1>
             <h2>Click on an image to earn points, but don't click on any more than once!</h2>
@@ -31,6 +77,7 @@ class App extends Component {
                 id={card.id}
                 key={card.id}
                 image={card.image}
+                checkCard={this.checkCard}
               />
             ))}
           </div>
